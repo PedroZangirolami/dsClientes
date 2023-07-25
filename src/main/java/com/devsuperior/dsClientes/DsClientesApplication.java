24d3dbc84623049f8297c81f7d88595f019925f4
PedroZangirolami <pedrozangirolami@gmail.com>
2023-07-25 15:50:35 -0300

    criando projeto

---
+++ src/main/java/com/devsuperior/dsClientes/DsClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsClientes/DsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
